cad07e9214aa395ca073b795654b85bd44a50cfa
Josman Proudinat <jmps2812@gmail.com>
2022-01-01 11:54:48 -0600

    Cambio en script de Google Analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -36,14 +36,23 @@ export default class MyDocument extends Document {
 
             <link rel='manifest' href='/manifest.json' />
 
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-GJNQ29CXE9"></script>
-            <script>
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-
-              gtag('config', 'G-GJNQ29CXE9');
-            </script>
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+              });
+            `,
+              }}
+            />
           </Head>
 
           <body>
